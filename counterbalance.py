20068af98105b3d5083bb22b165471382c59343f
Lukas Snoek <lukassnoek@gmail.com>
2017-09-13 19:11:14 +0200

    Almost working version; only categorical is not quite fixed

--- counterbalance.py
+++ counterbalance.py
@@ -1,186 +1,189 @@
 import numpy as np
-from scipy.stats import itemfreq
+from scipy.stats import ttest_ind, pearsonr
 from sklearn.model_selection import StratifiedKFold
 
 
 class CounterbalancedStratifiedSplit(object):
 
-    def __init__(self, X, y, c, counterbalance_tolerance=.05, n_splits=5,
-                 verbose=False, c_type='categorical'):
+    def __init__(self, X, y, c, n_splits=5,
+                 c_type='categorical', metric='corr', use_pval=False,
+                 threshold=0.05, verbose=False):
         self.X = X
         self.y = y
         self.c = c
-        self.counterbalance_tolerance = counterbalance_tolerance
         self.n_splits = n_splits
-        self.verbose = verbose
         self.c_type = c_type
+        self.metric = metric
+        self.use_pval = use_pval
+        self.threshold = threshold
+        self.seed = None
+        self.verbose = verbose
 
-        self.subsample_idx = np.arange(len(y))
-        self.y_classes = np.unique(y)
-
-        if c_type == 'categorical':
-            self.c_classes = np.unique(c)
-            self.n_confound_classes = len(self.c_classes)
-            self.counterbalanced_proportion = 1.0 / self.n_confound_classes
-
-        self._seed = None
-        self.checked_possible = False
-
-    def _check_categorical(self, y_class, idx, select_idx):
-
-        freqs = itemfreq(self.c[idx])
-
-        # Get absolute difference, diffs = distance-matrix
-        diffs = freqs[:, 1].reshape(-1, 1) - freqs[:, 1]
-
-        # If any absolute difference is larger than 0, we have an imbalance
-        if np.max(diffs) > 0:
-            print('Oops! The confound classes are not counterbalanced '
-                  'within y-class %s. Subsampling...' % str(y_class))
-
-            for c_class in self.c_classes:
-                # From self.subsample_idx, sample the idx of the number of
-                # y-class observations necessary to obtain a balanced class
-                isect = (self.y == y_class) & (self.c == c_class)
-                randsub = np.random.choice(self.subsample_idx[isect],
-                                           size=np.min(freqs[:, 1]),
-                                           replace=False)
-                select_idx = np.concatenate((select_idx, randsub))
-        else:
-            # If confound classes are counterbalanced within this y_class,
-            # select all of these idx
-            select_idx = np.concatenate((select_idx,
-                                         self.subsample_idx[idx]))
-
-        return select_idx
-
-    def _check_continuous(self):
-
-        from scipy.stats import ttest_ind
-        tstat, pval = ttest_ind(self.c[self.y == 0], self.c[self.y == 1])
-
-        c_y0 = self.c[self.y == 0]
-        c_y1 = self.c[self.y == 1]
-        select_idx = self.subsample_idx.copy()
-        idx_c_y0 = select_idx[self.y == 0]
-        idx_c_y1 = select_idx[self.y == 1]
-
-        while pval < 0.05 or c_y0.size < 3:
-
-            to_drop_c0 = c_y0.argmax() if tstat > 0 else c_y0.argmin()
-            idx_c_y0 = np.delete(idx_c_y0, to_drop_c0)
-            c_y0 = np.delete(c_y0, to_drop_c0)
-
-            to_drop_c1 = c_y1.argmax() if tstat < 0 else c_y0.argmin()
-            idx_c_y1 = np.delete(idx_c_y1, to_drop_c1)
-            c_y1 = np.delete(c_y1, to_drop_c1)
-            tstat, pval = ttest_ind(c_y0, c_y1)
-
-            if pval < 0.05:
-                success = True
-
-        return np.sort(np.concatenate((idx_c_y0, idx_c_y1))), pval
-
-    def check_possible(self):
-        """ Check if the confound classes are counterbalanced in each class of
-        y. If not, a subsampling index will be calculated such that they are.
-        """
-
-        if self.verbose:
-            print('Checking if a counterbalanced split is possible without '
-                  'subsampling...')
+    def _validate_fold(self, y, c):
 
         if self.c_type == 'continuous':
-            select_idx, pval = self._check_continuous()
 
-            if self.verbose:
-                print("Final p-value = %.3f (%i samples)" % (pval, select_idx.size))
+            if self.metric == 'corr':
+                stat, pval = pearsonr(c, y)
+            elif self.metric == 'tstat':
+                stat, pval = ttest_ind(c[y == 0], c[y == 1])
+            else:
+                raise ValueError("Please choose either 'corr' or 'tstat'!")
+
+            if self.use_pval:
+                return pval > self.threshold
+            else:
+                return np.abs(stat) < self.threshold
 
         elif self.c_type == 'categorical':
-            select_idx = np.array([])
-            for y_class in self.y_classes:
-                idx = self.y == y_class
-                select_idx = self._check_categorical(y_class, idx, select_idx)
 
-            if self.verbose:
-                print('After subsampling, proportions of c-classes in y_classes:')
-                y_tmp = self.y[self.subsample_idx]
-                c_tmp = self.c[self.subsample_idx]
+            bincounts = np.zeros((np.unique(y).size, np.unique(c).size))
+            for i, y_class in enumerate(np.unique(y)):
+                bincounts[i, :] = np.bincount(c[y == y_class])
 
-                for y_class in self.y_classes:
-                    print(itemfreq(c_tmp[y_tmp == y_class]))
+            counterbalanced = np.all(bincounts[0, :] == bincounts[1, :])
+            return counterbalanced
 
-        self.checked_possible = True
-        self.subsample_idx = np.sort(select_idx).astype(int)
+    def _subsample_continuous(self):
 
+        # First, let's do a t-test to check for differences between
+        # c | y=0 and c | y=1; thus, only binary c for now
+        this_c = self.c[self.subsample_idx]
+        this_y = self.y[self.subsample_idx]
 
-    def find_counterbalanced_seed(self, max_attempts=50000):
+        if self.metric == 'tstat':
+            stat, pval = ttest_ind(this_c[this_y == 0], this_c[this_y == 1])
+            stat *= -1  # To make it consistent with corr
+        else:
+            stat, pval = pearsonr(this_c, this_y)
+
+        if self.verbose:
+            stat_word = 'correlation' if self.metric == 'corr' else 'tstat'
+            print("Current overall %s c/y (pval): "
+                  "%.3f (%.3f)" % (stat_word, stat, pval))
+
+        c_y0 = this_c[this_y == 0]
+        c_y1 = this_c[this_y == 1]
+        idx_c_y0 = self.subsample_idx[this_y == 0]
+        idx_c_y1 = self.subsample_idx[this_y == 1]
+
+        to_drop_c0 = c_y0.argmax() if stat < 0 else c_y0.argmin()
+        idx_c_y0 = np.delete(idx_c_y0, to_drop_c0)
+        c_y0 = np.delete(c_y0, to_drop_c0)
+
+        to_drop_c1 = c_y1.argmax() if stat > 0 else c_y0.argmin()
+        idx_c_y1 = np.delete(idx_c_y1, to_drop_c1)
+        c_y1 = np.delete(c_y1, to_drop_c1)
+
+        self.subsample_idx = np.sort(np.concatenate((idx_c_y0, idx_c_y1)))
+
+    def _subsample_categorical(self):
+
+        c_unique = np.unique(self.c)
+        y_unique = np.unique(self.y)
+        counts = np.zeros((y_unique.size, c_unique.size))
+
+        # Count how many times a c appears for a given y
+        # so, count(c | y_i)
+        for i, y_class in enumerate(y_unique):
+            this_c = self.c[self.y == y_class]
+            counts[i, :] = np.array([(c == this_c).sum() for c in c_unique])
+
+        # ... yielding a len(y_unique) x len(c_unique) matrix
+        # Now, take the minimum across rows
+        min_counts = counts.min(axis=0)
+
+        if np.all(min_counts == 0):
+            msg = ("Wow, your data is really messed up ... There is no way to "
+                   "subsample it, because the minimum proportion of all values"
+                   "of c across all values of y is 0 ...")
+            raise ValueError(msg)
+
+        # Which are exactly the number of trials (per c) which you need to
+        # subsample
+        final_idx = []
+        for i, y_class in enumerate(y_unique):
+
+            this_idx = self.subsample_idx[self.y == y_class]
+            this_c = self.c[self.y == y_class]
+
+            for ii, c in enumerate(c_unique):
+                final_idx.append(np.random.choice(this_idx[this_c == c],
+                                                  int(min_counts[ii]),
+                                                  replace=False))
+
+        self.subsample_idx = np.sort(np.concatenate(final_idx))
+
+    def _subsample(self):
+
+        if self.c_type == 'continuous':
+            self._subsample_continuous()
+        elif self.c_type == 'categorical':
+            self._subsample_categorical()
+        else:
+            raise ValueError("Please pick c_type='categorical' or "
+                             "c_type='continuous'")
+
+        if len(self.subsample_idx) < (2 * len(np.unique(self.y))):
+            msg = ("Probably subsampled too much (only have %i samples now); "
+                   "this dataset can't be meaningfully "
+                   "counterbalanced" % len(self.subsample_idx))
+            raise ValueError(msg)
+
+    def _find_counterbalanced_seed(self, max_attempts=1000):
         """ Find a seed of Stratified K-Fold that gives counterbalanced
         classes """
 
-        if not self.checked_possible:
-            self.check_possible()
+        y_tmp = self.y[self.subsample_idx]
+        c_tmp = self.c[self.subsample_idx]
+        X_tmp = self.X[self.subsample_idx]
+        lowest_y_count = np.min(np.bincount(y_tmp))
 
-        X_tmp = self.X.copy()[self.subsample_idx]
-        y_tmp = self.y.copy()[self.subsample_idx]
-        c_tmp = self.c.copy()[self.subsample_idx]
+        if lowest_y_count < self.n_splits:
+            raise ValueError("You have to few samples of each class of y for "
+                             "n_splits=%i; highest number of splits you can "
+                             "use is %i" % (self.n_splits, lowest_y_count))
 
         seeds = np.random.randint(0, high=1e7, size=max_attempts, dtype=int)
+
         for i, seed in enumerate(seeds):
             skf = StratifiedKFold(n_splits=self.n_splits, shuffle=True,
                                   random_state=seed)
-            bad_split = False
+
             for (train_idx, test_idx) in skf.split(X_tmp, y_tmp):
 
-                if bad_split:
-                    if self.verbose and i % 100 == 0:
-                        print('.', end='')
+                this_y, this_c = y_tmp[train_idx], c_tmp[train_idx]
+                good_split = self._validate_fold(this_y, this_c)
+
+                if not good_split:
                     break
 
-                for set_ in (train_idx, test_idx):
-                    y_set = y_tmp[set_]
-                    c_set = c_tmp[set_]
-
-
-
-                    for y_class in self.y_classes:
-                        c_set_class = c_set[y_set == y_class]
-
-                        if self.c_type == 'categorical':
-                            bad_split = self._check_fold_c_categorical(c_set_class)
-
-            if not bad_split:
-                if self.verbose:
-                    print('\nGood split found! Seed %d was used.' % i)
+            if good_split:
+                print("Picking seed %i" % seed)
                 self.seed = seed
-                break
+                return good_split
 
-        if self.seed is None:
-            raise(ValueError('\nSorry, could not find any good split...'))
-
-    def _check_fold_c_continuous():
-        pass
-
-    def _check_fold_c_categorical(self, c_set_class):
-        # Get proportions of each class of c for each y_class
-        freqs = itemfreq(c_set_class).astype(float)[:, 1]
-        proportions = freqs / len(c_set_class)
-
-        # Check whether the proportions are equal
-        diffs = proportions.reshape(-1, 1) - proportions
-        maxdiffs = np.max(diffs)
-        reject = maxdiffs > self.counterbalance_tolerance
-        if proportions.shape[0] == 1 or reject:
-            return True
-        else:
-            return False
+        return good_split
 
     def split(self, X, y):
         """ The final idx to output are subsamples of the subsample_idx... """
 
-        if self.seed is None:
-            raise(ValueError('You need to run CounterbalancedStratifiedSplit.'
-                             'find_counterbalanced_seed() first'))
+        self.subsample_idx = np.arange(self.y.size)
+
+        if self.c_type == 'continuous':
+            found_split = self._find_counterbalanced_seed()
+            while not found_split:
+                self._subsample()
+                found_split = self._find_counterbalanced_seed()
+        elif self.c_type == 'categorical':
+            self._subsample()
+            found_split = self._find_counterbalanced_seed()
+
+        if self.verbose:
+            new_N = float(len(self.subsample_idx))
+            old_N = self.y.size
+            print("Size of y after subsampling: %i (%.1f percent reduction in "
+                  "samples)" % (new_N, (old_N - new_N) / old_N * 100))
 
         X_tmp = X[self.subsample_idx]
         y_tmp = y[self.subsample_idx]
@@ -201,19 +204,17 @@ if __name__ == '__main__':
 
     n_half = int(n_samp / 2)
     y = np.repeat([0, 1], repeats=n_half)
-    c = np.roll(y, 10) + np.random.randn(y.size)
+    c = np.roll(y, 10)
+    X = np.random.randn(n_samp, n_feat)
 
-    data = np.random.randn(n_samp, n_feat)
-    data[c == 1, :] += 5
-    data[y == 1, :] += 5.5
-    X = data
+    css = CounterbalancedStratifiedSplit(X, y, c, n_splits=3,
+                                         c_type='categorical', verbose=True,
+                                         metric='tstat', threshold=1)
+    folds = css.split(X, y)
 
-    css = CounterbalancedStratifiedSplit(X, y, c, n_splits=5, verbose=True,
-                                         counterbalance_tolerance=.05,
-                                         c_type='continuous')
-    css.check_possible()
-    y_tmp = y[css.subsample_idx]
-    c_tmp = c[css.subsample_idx]
-
-    css.find_counterbalanced_seed()
-    splts = css.split(X, y)
+    for train_idx, test_idx in folds:
+        this_c = c[train_idx]
+        this_y = y[train_idx]
+        print(this_c[this_y == 0])
+        print(this_c[this_y == 1])
+        print(pearsonr(this_c, this_y))
